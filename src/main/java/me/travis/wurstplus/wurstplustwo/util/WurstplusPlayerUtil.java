d2bbd2c6d51ada51ade6ab0176bbc858feb4c5f4
Trambled <67767001+Trambled@users.noreply.github.com>
2021-01-06 09:40:57 -0500

    add burrowtp for auroraanarchy

--- src/main/java/me/travis/wurstplus/wurstplustwo/util/WurstplusPlayerUtil.java
+++ src/main/java/me/travis/wurstplus/wurstplustwo/util/WurstplusPlayerUtil.java
@@ -48,6 +48,27 @@ public class WurstplusPlayerUtil  //merged with past
             }
         }
     }
+
+    public static double getDirection() {
+        float rotationYaw = mc.player.rotationYaw;
+        if (mc.player.moveForward < 0.0f) {
+            rotationYaw += 180.0f;
+        }
+        float forward = 1.0f;
+        if (mc.player.moveForward < 0.0f) {
+            forward = -0.5f;
+        }
+        else if (mc.player.moveForward > 0.0f) {
+            forward = 0.5f;
+        }
+        if (mc.player.moveStrafing > 0.0f) {
+            rotationYaw -= 90.0f * forward;
+        }
+        if (mc.player.moveStrafing < 0.0f) {
+            rotationYaw += 90.0f * forward;
+        }
+        return Math.toRadians(rotationYaw);
+    }
     
     public double getMoveYaw() {
         float strafe = 90.0f * WurstplusPlayerUtil.mc.player.moveStrafing;
