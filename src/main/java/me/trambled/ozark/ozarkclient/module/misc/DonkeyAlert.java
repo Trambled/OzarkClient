ccbc37a9df7791b91af617ef8d3a8146235386b1
Trambled <67767001+Trambled@users.noreply.github.com>
2021-03-29 20:22:51 -0400

    building works now and some other shit

--- src/main/java/me/trambled/ozark/ozarkclient/module/misc/DonkeyAlert.java
+++ src/main/java/me/trambled/ozark/ozarkclient/module/misc/DonkeyAlert.java
@@ -9,8 +9,10 @@ import me.zero.alpine.fork.listener.EventHandler;
 import me.zero.alpine.fork.listener.Listener;
 import net.minecraft.client.audio.PositionedSoundRecord;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.monster.EntitySlime;
 import net.minecraft.entity.passive.EntityDonkey;
 import net.minecraft.entity.passive.EntityLlama;
+import net.minecraft.entity.passive.EntityMule;
 import net.minecraft.init.SoundEvents;
 
 import java.util.ArrayList;
@@ -28,6 +30,8 @@ public class DonkeyAlert extends Module
 
     Setting donkeys = create("Donkeys", "AlertDonkeys", true);
     Setting llamas = create("Llamas", "AlertLlamas", true);
+    Setting mules = create("Mules", "AlertMules", true);
+    Setting slimes = create("Slimes", "AlertSlimes", true);
     Setting sound = create("Sound", "AlertSound", true);
 
     List<Entity> entities = new ArrayList<>();
@@ -58,7 +62,29 @@ public class DonkeyAlert extends Module
                     if (sound.get_value(true)) {
                         mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f));
                     }
-                    MessageUtil.send_client_message("Donkey found at " + Math.round(e.posX) + ", " + Math.round(e.posZ) + "!");
+                    MessageUtil.send_client_message("Llama found at " + Math.round(e.posX) + ", " + Math.round(e.posZ) + "!");
+                    entities.add(e);
+                }
+            }
+        }
+        if (mules.get_value(true)) {
+            for (final Entity e : mc.world.loadedEntityList) {
+                if (e instanceof EntityMule && !entities.contains(e)) {
+                    if (sound.get_value(true)) {
+                        mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f));
+                    }
+                    MessageUtil.send_client_message("Mule found at " + Math.round(e.posX) + ", " + Math.round(e.posZ) + "!");
+                    entities.add(e);
+                }
+            }
+        }
+        if (slimes.get_value(true)) {
+            for (final Entity e : mc.world.loadedEntityList) {
+                if (e instanceof EntitySlime && !entities.contains(e)) {
+                    if (sound.get_value(true)) {
+                        mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f));
+                    }
+                    MessageUtil.send_client_message("Slime found at " + Math.round(e.posX) + ", " + Math.round(e.posZ) + "!");
                     entities.add(e);
                 }
             }
@@ -67,7 +93,7 @@ public class DonkeyAlert extends Module
 
     @EventHandler
     private final Listener<EventEntityRemoved> on_entity_removed = new Listener<>(event -> {
-        if ((event.get_entity() instanceof EntityDonkey && donkeys.get_value(true)) || (event.get_entity() instanceof EntityLlama && llamas.get_value(true))) {
+        if ((event.get_entity() instanceof EntityDonkey && donkeys.get_value(true)) || (event.get_entity() instanceof EntityLlama && llamas.get_value(true)) || (event.get_entity() instanceof EntityMule && mules.get_value(true)) || (event.get_entity() instanceof EntitySlime && slimes.get_value(true))) {
             entities.remove(event.get_entity());
         }
     });
