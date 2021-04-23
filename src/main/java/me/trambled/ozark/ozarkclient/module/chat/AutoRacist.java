ae21f119df4f220b55f41bf6e7493d7fdbb270c0
Trambled <67767001+Trambled@users.noreply.github.com>
2021-04-23 10:13:37 -0400

    finally fixed

--- src/main/java/me/trambled/ozark/ozarkclient/module/chat/AutoRacist.java
+++ src/main/java/me/trambled/ozark/ozarkclient/module/chat/AutoRacist.java
@@ -11,7 +11,7 @@ import java.util.Random;
 
 public class AutoRacist extends Module {
     
-    public ChatSpammer() {
+    public AutoRacist() {
         super(Category.CHAT);
 
         this.name = "AutoRacist";
