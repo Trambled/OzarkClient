534fa09e75b984e574508de946cde2cabd6830bc
x3th0 <71413668+x3th0@users.noreply.github.com>
2021-04-24 10:10:59 -0400

    Update ModuleManager.java

--- src/main/java/me/trambled/ozark/ozarkclient/manager/ModuleManager.java
+++ src/main/java/me/trambled/ozark/ozarkclient/manager/ModuleManager.java
@@ -119,6 +119,7 @@ public class ModuleManager {
 		add_module(new Anchor());
 		add_module(new AntiLevitation());
 		add_module(new EntitySpeed());
+		add_module(new ReverseStep());
 
 		// Render.
 		add_module(new Highlight());
