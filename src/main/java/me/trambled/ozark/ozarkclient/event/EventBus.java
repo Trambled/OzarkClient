8c22a6511dcb8eb02fc49b5b4f2a22c6f9a6a162
Trambled <67767001+Trambled@users.noreply.github.com>
2021-04-23 20:25:41 -0400

    git is retarded

--- src/main/java/me/trambled/ozark/ozarkclient/event/EventBus.java
+++
@@ -1,7 +0,0 @@
-package me.trambled.ozark.ozarkclient.event;
-
-import me.zero.alpine.fork.bus.EventManager;
-
-public class Eventbus {
-	public static final me.zero.alpine.fork.bus.EventBus EVENT_BUS = new EventManager();
-}
