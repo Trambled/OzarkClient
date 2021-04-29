a9633e95dea537f3b24ec982c4aa0f2b3c9f0d6b
Trambled <67767001+Trambled@users.noreply.github.com>
2021-04-29 09:30:09 -0400

    make this readable

--- src/main/java/me/trambled/ozark/ozarkclient/util/DiscordUtil.java
+++ src/main/java/me/trambled/ozark/ozarkclient/util/DiscordUtil.java
@@ -37,91 +37,90 @@ public class DiscordUtil
                     DiscordUtil.rpc.Discord_RunCallbacks();
                     DiscordUtil.details = "";
                     DiscordUtil.state = "";
-
-                    if (Ozark.get_setting_manager().get_setting_with_tag("DiscordRPC", "RPCMode").in("Normal")) {
-                        DiscordUtil.presence.largeImageKey = "ozark_2";
-                    } else {
-                        DiscordUtil.presence.largeImageKey = "lempity";
-                    }
+                    	if (Ozark.get_setting_manager().get_setting_with_tag("DiscordRPC", "RPCMode").in("Normal")) {
+                        	DiscordUtil.presence.largeImageKey = "ozark_2";
+                    	} else {
+                        	DiscordUtil.presence.largeImageKey = "lempity";
+                    	}
 					
-					if (mc.world == null) {
-						DiscordUtil.details = "In the menus";
-                        DiscordUtil.presence.smallImageKey = "troll";
-
-                        //idk i just wanted it to do everything lol
-						if (mc.currentScreen instanceof GuiWorldSelection) {
-							DiscordUtil.state = "Selecting a world to play on";
-						} else if (mc.currentScreen instanceof GuiMainMenu) {
-                            DiscordUtil.state = "In the Main Menu";
-                        } else if (mc.currentScreen instanceof GuiOptions) {
-                            DiscordUtil.state = "Configuring options";
-						} else if (mc.currentScreen instanceof GuiMultiplayer || mc.currentScreen instanceof GuiScreenAddServer || mc.currentScreen instanceof GuiScreenServerList) {
-							DiscordUtil.state = "Selecting a server to play on";
-						} else if (mc.currentScreen instanceof GuiScreenResourcePacks) {
-                            DiscordUtil.state = "Selecting a resource pack";
-                        } else if (mc.currentScreen instanceof GuiDisconnected) {
-                            DiscordUtil.state = "Disconnected from server :c";
-                        } else if (mc.currentScreen instanceof GuiConnecting) {
-                            DiscordUtil.state = "Connecting to a server";
-                        } else if (mc.currentScreen instanceof GuiCreateFlatWorld || mc.currentScreen instanceof GuiCreateWorld) {
-                            DiscordUtil.state = "Creating world";
+		    	if (mc.world == null) {
+				DiscordUtil.details = "In the menus";
+                        	DiscordUtil.presence.smallImageKey = "troll";
+				//idk i just wanted it to do everything lol
+		    		if (mc.currentScreen instanceof GuiWorldSelection) {
+					DiscordUtil.state = "Selecting a world to play on";
+		 		} else if (mc.currentScreen instanceof GuiMainMenu) {
+                            		DiscordUtil.state = "In the Main Menu";
+                        	} else if (mc.currentScreen instanceof GuiOptions) {
+                            		DiscordUtil.state = "Configuring options";
+				} else if (mc.currentScreen instanceof GuiMultiplayer || mc.currentScreen instanceof GuiScreenAddServer || mc.currentScreen instanceof GuiScreenServerList) {
+					DiscordUtil.state = "Selecting a server to play on";
+				} else if (mc.currentScreen instanceof GuiScreenResourcePacks) {
+                            		DiscordUtil.state = "Selecting a resource pack";
+                        	} else if (mc.currentScreen instanceof GuiDisconnected) {
+                            		DiscordUtil.state = "Disconnected from server :c";
+                        	} else if (mc.currentScreen instanceof GuiConnecting) {
+                            		DiscordUtil.state = "Connecting to a server";
+                        	} else if (mc.currentScreen instanceof GuiCreateFlatWorld || mc.currentScreen instanceof GuiCreateWorld) {
+                            		DiscordUtil.state = "Creating world";
+                       		} else {
+					// im p sure i covered everything except the exact options
+                            		// so ig this is heere
+					DiscordUtil.state = "Configuring options";
+				}
                         } else {
-						    // im p sure i covered everything except the exact options
-                            // so ig this is here
-						    DiscordUtil.state = "Configuring options";
-                        }
-					} else {
-						if (mc.player != null) {
-						    int health = Math.round(mc.player.getHealth()+mc.player.getAbsorptionAmount());
-							DiscordUtil.state = health + " HP";
-							if (mc.isIntegratedServerRunning()) {
-								DiscordUtil.details = "Playing Singleplayer";
-							} else if (!mc.isIntegratedServerRunning()) {
-								DiscordUtil.details = "Playing " + mc.getCurrentServerData().serverIP;
-
-								if (mc.getCurrentServerData().serverIP.equals("aurorapvp.club") || mc.getCurrentServerData().serverIP.equals("auroraanarchy.org")) {
-                                    // doesnt like aurora and aurora anarchy have a slightly different logo?
-                                    // either way idc lol
-								    DiscordUtil.presence.smallImageKey = "aurora";
-                                } else if (mc.getCurrentServerData().serverIP.equals("8b8t.xyz")) {
-                                    DiscordUtil.presence.smallImageKey = "8b8t";
-                                } else if (mc.getCurrentServerData().serverIP.equals("0b0t.org")) {
-                                    DiscordUtil.presence.smallImageKey = "0b0t";
-                                } else if (mc.getCurrentServerData().serverIP.equals("5b5t.org")) {
-                                    DiscordUtil.presence.smallImageKey = "5b5t";
-                                } else if (mc.getCurrentServerData().serverIP.equals("9b9t.com") || mc.getCurrentServerData().serverIP.equals("9b9t.org")) {
-                                    DiscordUtil.presence.smallImageKey = "9b9t";
-                                } else if (mc.getCurrentServerData().serverIP.equals("bedtrap.org")) {
-                                    DiscordUtil.presence.smallImageKey = "bedtrap";
-                                } else if (mc.getCurrentServerData().serverIP.equals("constantiam.net")) {
-                                    DiscordUtil.presence.smallImageKey = "const";
-                                } else if (mc.getCurrentServerData().serverIP.equals("l2x9.org")) {
-                                    DiscordUtil.presence.smallImageKey = "l2";
-                                } else if (mc.getCurrentServerData().serverIP.equals("matrixanarchy.net")) {
-                                    DiscordUtil.presence.smallImageKey = "matrix";
-                                } else if (mc.getCurrentServerData().serverIP.equals("cpe2.ign.gg")) {
-                                    DiscordUtil.presence.smallImageKey = "cpe";
-                                } else if (mc.getCurrentServerData().serverIP.equals("oldfag.org")) {
-                                    DiscordUtil.presence.smallImageKey = "oldfag";
-                                } else if (mc.getCurrentServerData().serverIP.equals("openanarchy.org")) {
-                                    DiscordUtil.presence.smallImageKey = "oa";
-                                } else if (mc.getCurrentServerData().serverIP.equals("eliteanarchy.org")) {
-                                    DiscordUtil.presence.smallImageKey = "elite";
-                                } else if (mc.getCurrentServerData().serverIP.equals("6b6t.co")) {
-                                    DiscordUtil.presence.smallImageKey = "6b6t";
-                                } else if (mc.getCurrentServerData().serverIP.equals("2b2t.org")) {
-                                    DiscordUtil.presence.smallImageKey = "2b2t";
-                                } else if (mc.getCurrentServerData().serverIP.equals("2b2tpvp.net")) {
-                                    DiscordUtil.presence.smallImageKey = "2bpvp";
-                                } else if (mc.getCurrentServerData().serverIP.equals("us.crystalpvp.cc") || mc.getCurrentServerData().serverIP.equals("crystalpvp.cc")) {
-                                    DiscordUtil.presence.smallImageKey = "cc";
-                                } else {
-								    //trol
-                                    DiscordUtil.presence.smallImageKey = "troll";
-                                }
-							}
-						}
+				if (mc.player != null) {
+					int health = Math.round(mc.player.getHealth()+mc.player.getAbsorptionAmount());
+					DiscordUtil.state = health + " HP";
+					if (mc.isIntegratedServerRunning()) {
+						DiscordUtil.details = "Playing Singleplayer";
+					} else if (!mc.isIntegratedServerRunning()) {
+						DiscordUtil.details = "Playing " + mc.getCurrentServerData().serverIP;
 					}
+
+					if (mc.getCurrentServerData().serverIP.equals("aurorapvp.club") || mc.getCurrentServerData().serverIP.equals("auroraanarchy.org")) {
+                                    		// doesnt like aurora and aurora anarchy have a slightly different logo?
+                                    		// either way idc lol
+						DiscordUtil.presence.smallImageKey = "aurora";
+                                	} else if (mc.getCurrentServerData().serverIP.equals("8b8t.xyz")) {
+                                    		DiscordUtil.presence.smallImageKey = "8b8t";
+                                	} else if (mc.getCurrentServerData().serverIP.equals("0b0t.org")) {
+                                    		DiscordUtil.presence.smallImageKey = "0b0t";
+                                	} else if (mc.getCurrentServerData().serverIP.equals("5b5t.org")) {
+                                    		DiscordUtil.presence.smallImageKey = "5b5t";
+                                	} else if (mc.getCurrentServerData().serverIP.equals("9b9t.com") || mc.getCurrentServerData().serverIP.equals("9b9t.org")) {
+                                    		DiscordUtil.presence.smallImageKey = "9b9t";
+                                	} else if (mc.getCurrentServerData().serverIP.equals("bedtrap.org")) {
+                                    		DiscordUtil.presence.smallImageKey = "bedtrap";
+                                	} else if (mc.getCurrentServerData().serverIP.equals("constantiam.net")) {
+                                    		DiscordUtil.presence.smallImageKey = "const";
+                                	} else if (mc.getCurrentServerData().serverIP.equals("l2x9.org")) {
+                                    		DiscordUtil.presence.smallImageKey = "l2";
+                                	} else if (mc.getCurrentServerData().serverIP.equals("matrixanarchy.net")) {
+                                    		DiscordUtil.presence.smallImageKey = "matrix";
+                                	} else if (mc.getCurrentServerData().serverIP.equals("cpe2.ign.gg")) {
+                                    		DiscordUtil.presence.smallImageKey = "cpe";
+                                	} else if (mc.getCurrentServerData().serverIP.equals("oldfag.org")) {
+                                   		DiscordUtil.presence.smallImageKey = "oldfag";
+                                	} else if (mc.getCurrentServerData().serverIP.equals("openanarchy.org")) {
+                                    		DiscordUtil.presence.smallImageKey = "oa";
+                                	} else if (mc.getCurrentServerData().serverIP.equals("eliteanarchy.org")) {
+                                    		DiscordUtil.presence.smallImageKey = "elite";
+                                	} else if (mc.getCurrentServerData().serverIP.equals("6b6t.co")) {
+                                    		DiscordUtil.presence.smallImageKey = "6b6t";
+                                	} else if (mc.getCurrentServerData().serverIP.equals("2b2t.org")) {
+                                    		DiscordUtil.presence.smallImageKey = "2b2t";
+                                	} else if (mc.getCurrentServerData().serverIP.equals("2b2tpvp.net")) {
+                                    		DiscordUtil.presence.smallImageKey = "2bpvp";
+                                	} else if (mc.getCurrentServerData().serverIP.equals("us.crystalpvp.cc") || mc.getCurrentServerData().serverIP.equals("crystalpvp.cc")) {
+                                    		DiscordUtil.presence.smallImageKey = "cc";
+                                	} else {
+						//trol
+                                    		DiscordUtil.presence.smallImageKey = "troll";
+                                	}
+				}
+			}
+						
 
                     DiscordUtil.presence.details = DiscordUtil.details;
                     DiscordUtil.presence.state = DiscordUtil.state;
