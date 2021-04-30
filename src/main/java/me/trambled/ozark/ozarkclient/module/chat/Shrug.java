111f945c9802522c870b0d819868884ab60d42ce
notperry1234567890 <65968863+notperry1234567890@users.noreply.github.com>
2021-04-29 18:04:22 -0700

    oop funni

--- src/main/java/me/trambled/ozark/ozarkclient/module/chat/Shrug.java
+++ src/main/java/me/trambled/ozark/ozarkclient/module/chat/Shrug.java
@@ -10,7 +10,7 @@ public class Shrug extends Module {
       
         this.name = "Shrug";
         this.tag = "Shrug";
-        this.description = "¯\_(ツ)_/¯";
+        this.description = "\u00AF\\_(\u30C4)_/\u00AF";
       
     }
   
