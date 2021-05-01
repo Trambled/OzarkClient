c0dfe7709af2435a97b5f293216088cdc0da75d1
notperry1234567890 <65968863+notperry1234567890@users.noreply.github.com>
2021-04-30 21:43:52 -0700

    so it actually builds kambing

--- src/main/java/me/trambled/ozark/ozarkclient/module/chat/AutoDDOS.java
+++ src/main/java/me/trambled/ozark/ozarkclient/module/chat/AutoDDOS.java
@@ -4,7 +4,6 @@ import me.trambled.ozark.ozarkclient.util.MessageUtil;
 import com.mojang.realmsclient.gui.ChatFormatting;
 import me.trambled.ozark.ozarkclient.module.Category;
 import me.trambled.ozark.ozarkclient.module.Module;
-import net.minecraft.client.Minecraft;
 
 public class AutoDDOS extends Module {
     public AutoDDOS() {
@@ -19,11 +18,10 @@ public class AutoDDOS extends Module {
     @Override 
     protected void enable() {
         MessageUtil.send_client_message("Successfully pinged IP address [REDACTED] with 32 bytes of data.");
-	MessageUtil.send_client_message("");  
-	MessageUtil.send_client_message("ass larp");    
-	MessageUtil.send_client_message("Yo i think we ddos the wrong person");   
-	MessageUtil.send_client_message(mc.player.getName + "is this ign urs?");
-	MessageUtil.send_client_message("Error: Ping is 1827, closing Minecraft");
-	set_disable();
-    }		    
+    }
+
+    @Override 
+    public void update() {
+        MessageUtil.send_client_message("");
+    }
 }
