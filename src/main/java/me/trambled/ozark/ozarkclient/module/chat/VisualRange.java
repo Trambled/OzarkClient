76a2d4acbe6406374a7a3592f292849a73bdb155
notperry1234567890 <65968863+notperry1234567890@users.noreply.github.com>
2021-05-09 18:09:45 -0700

    visualrange fix (trambled monke)

--- src/main/java/me/trambled/ozark/ozarkclient/module/chat/VisualRange.java
+++ src/main/java/me/trambled/ozark/ozarkclient/module/chat/VisualRange.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-<<<<<<< HEAD
 public
 class VisualRange extends Module {
 
@@ -58,51 +57,5 @@ class VisualRange extends Module {
                 }
             }
         }
-
     }
-=======
-public class VisualRange extends Module {
-
-	private List<String> people;
-
-	public VisualRange() {
-		super(Category.CHAT);
-
-		this.name        = "Visual Range";
-		this.tag         = "VisualRange";
-		this.description = "bc using ur eyes is overrated";
-	}
-
-	@Override
-	public void enable() {
-		people = new ArrayList<>();
-	}
-
-	@Override
-	public void update() {
-		if (mc.world == null | mc.player == null) return;
-
-		List<String> peoplenew = new ArrayList<>();
-		List<EntityPlayer> playerEntities = mc.world.playerEntities;
-
-		for (Entity e : playerEntities) {
-			if (e.getName().equals(mc.player.getName())) continue;
-			peoplenew.add(e.getName());
-		}
-
-		if (peoplenew.size() > 0) {
-			for (String name : peoplenew) {
-				if (!people.contains(name)) {
-					if (FriendUtil.isFriend(name)) {
-						MessageUtil.send_client_message("I see a friend named " + ChatFormatting.RESET + ChatFormatting.GREEN + name + ChatFormatting.RESET + ". Say Hi!");
-					} else {
-						MessageUtil.send_client_message("I see an enemy named " + ChatFormatting.RESET + ChatFormatting.RED + name + ChatFormatting.RESET + "");
-					}
-					people.add(name);
-				}
-			}
-		}
-
-	}
->>>>>>> parent of 6988c08 (woops uwu :trollface:)
 }
