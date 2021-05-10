d8169df9e7b151050bcecfaefc6ce559df65b1ed
Trambled <67767001+Trambled@users.noreply.github.com>
2021-05-10 08:36:45 -0400

    Update BurrowESP.java

--- src/main/java/me/trambled/ozark/ozarkclient/module/render/BurrowESP.java
+++ src/main/java/me/trambled/ozark/ozarkclient/module/render/BurrowESP.java
@@ -10,7 +10,6 @@ import me.trambled.ozark.ozarkclient.util.FriendUtil;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
-import me.trambled.ozark.ozarkclient.util.MessageUtil;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -31,8 +30,8 @@ public class BurrowESP extends Module {
 
     Setting render_mode = create("Render Mode", "BurrowRenderMode", "Pretty", combobox("Pretty", "Solid", "Outline"));
     Setting r = create("R", "BurrowR", 255, 0, 255);
-	Setting g = create("G", "BurrowG", 255, 0, 255);
-	Setting b = create("B", "BurrowB", 255, 0, 255);
+    Setting g = create("G", "BurrowG", 255, 0, 255);
+    Setting b = create("B", "BurrowB", 255, 0, 255);
     Setting a = create("A", "BurrowA", 100, 0, 255);
 
     private boolean solid;
@@ -76,10 +75,9 @@ public class BurrowESP extends Module {
 
         //we need to add the 0.2 to prevent it from false flagging shifting
         BlockPos pos = new BlockPos(player.posX, player.posY + 0.2, player.posZ);
-//trambled pls add an option to disable this notifier because my small brain cant
+	//trambled pls add an option to disable this notifier because my small brain cant
         if (mc.world.getBlockState(pos).getBlock().equals(Blocks.OBSIDIAN)) {
-		MessageUtil.send_client_message("Burrowfag detected");
-            return true;
+              return true;
         } 
 
         return false;
@@ -87,7 +85,7 @@ public class BurrowESP extends Module {
     }
 
     @Override
-	public void render(EventRender event) {
+    public void render(EventRender event) {
 
         if (render_mode.in("Pretty")) {
             outline = true;
