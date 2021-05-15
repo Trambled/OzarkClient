833f1706ac2b5a82443c197f7187155f0472cd21
Listed7 <77821639+Listed7@users.noreply.github.com>
2021-05-15 21:44:24 +0900

    Update PyroGUI.java

--- src/main/java/me/trambled/ozark/ozarkclient/module/gui/PyroGUI.java
+++ src/main/java/me/trambled/ozark/ozarkclient/module/gui/PyroGUI.java
@@ -9,6 +9,7 @@ import java.net.URI;
 import me.trambled.ozark.ozarkclient.util.MessageUtil;
 
 //ultimate troll module by profkambing :trollface: cope more idiots!!!
+//such a shit troll kys
 public class PyroGUI extends Module {
     public PyroGUI() {
         super(Category.GUI);
@@ -19,17 +20,17 @@ public class PyroGUI extends Module {
         this.toggle_message = false;
     }
 
-    public void enable() {
-        if (mc.player != null)
-            MessageUtil.send_client_message("EZZZ KID COPE MORE HAHAHAHAHHA GOTEM");
-        mc.player.sendChatMessage("/kill");
-        mc.player.sendChatMessage("I am a pedophile, a larper, a newfag, a fitfag, a nigger, a jew, and a retard");
-            try {
-                Desktop.getDesktop().browse(URI.create("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
-            } catch (Exception ignored) {
-            }
-        set_disable();
-    }
+   // public void enable() {
+       // if (mc.player != null)
+            //MessageUtil.send_client_message("EZZZ KID COPE MORE HAHAHAHAHHA GOTEM");
+       // mc.player.sendChatMessage("/kill");
+       // mc.player.sendChatMessage("I am a pedophile, a larper, a newfag, a fitfag, a nigger, a jew, and a retard");
+          //  try {
+              //  Desktop.getDesktop().browse(URI.create("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
+           // } catch (Exception ignored) {
+          //  }
+       // set_disable();
+   // }
 }
 
 
