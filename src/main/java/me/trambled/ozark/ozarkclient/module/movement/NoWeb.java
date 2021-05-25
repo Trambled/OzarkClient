753664cb4b0c0c7e32c4d5de25b502082cb24f83
ProfKambing <80607981+ProfKambing@users.noreply.github.com>
2021-05-25 18:51:51 +0800

    fixed because im a retard (im now god at java)

--- src/main/java/me/trambled/ozark/ozarkclient/module/movement/NoWeb.java
+++ src/main/java/me/trambled/ozark/ozarkclient/module/movement/NoWeb.java
@@ -4,9 +4,6 @@ import me.trambled.ozark.ozarkclient.module.Category;
 import me.trambled.ozark.ozarkclient.module.Module;
 import me.trambled.ozark.ozarkclient.module.Setting;
 import me.trambled.ozark.ozarkclient.util.MessageUtil;
-import net.minecraft.init.Blocks;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.block.material.Material;
 
 //KAMBING MADE THIS COPYRIGHT CODENAME_KAMBING#0001 ON DISCORD NO SKIDDING OR GAY I CAN SUE UR MOM (listed google this code!! i made this ok?!??!)
@@ -21,23 +18,24 @@ public class NoWeb extends Module {
     }
 
     Setting speed = create("Speed", "TimerSpeed", 4f, 0.1f, 10f);
-    Setting notify = create("Notify", "Notify",false);
+    Setting notify = create("Notify", "Notify", false);
 
     @Override
     public void update() {
         if (mc.world.isMaterialInBB(mc.player.getEntityBoundingBox().grow(-0.10000000149011612D, -0.4000000059604645D, -0.10000000149011612D), Material.WEB)) {
             mc.timer.tickLength = 50.0f / ((speed.get_value(1) == 0f) ? 0.1f : speed.get_value(1));
             if (notify.get_value(true)) {
-                MessageUtil.send_client_message("You're in a web!");
+                MessageUtil.send_client_message("You're in a web");
+            }
+
+            if ((!mc.world.isMaterialInBB(mc.player.getEntityBoundingBox().grow(-0.10000000149011612D, -0.4000000059604645D, -0.10000000149011612D), Material.WEB))) {
+                mc.timer.tickLength = 50.0f;
             }
         }
     }
-    @Override
-    protected void disable () {
-        mc.timer.tickLength = 50.0f;
     }
 
-}
+
 
 
 
