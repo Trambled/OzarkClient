a8bed43d3517fa320120e51438579a4fd0a34222
x3th0 <71413668+x3th0@users.noreply.github.com>
2021-06-07 12:25:31 -0400

    Create AutoEthoDupe.java

---
+++ src/main/java/me/trambled/ozark/ozarkclient/module/misc/AutoEthoDupe.java
@@ -0,0 +1,56 @@
+package me.trambled.ozark.ozarkclient.module.misc;
+
+import com.mojang.authlib.GameProfile;
+import me.trambled.ozark.ozarkclient.module.Category;
+import me.trambled.ozark.ozarkclient.module.Module;
+import me.trambled.ozark.ozarkclient.util.MessageUtil;
+import me.zero.alpine.fork.listener.EventHandler;
+import me.zero.alpine.fork.listener.Listener;
+import net.minecraft.client.entity.EntityOtherPlayerMP;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+
+import java.util.UUID;
+
+public class AutoEthoDupe extends Module {
+    
+    public AutoEthoDupe() {
+        super(Category.MISC);
+
+		this.name        = "AutoEthoDupe";
+		this.tag         = "AutoEthoDupe";
+		this.description = "etho dupe snine19 tudou love 2017 classic meme";
+    }
+
+    private EntityOtherPlayerMP fake_player;
+
+    @Override
+    protected void enable() {
+        
+        fake_player = new EntityOtherPlayerMP(mc.world, new GameProfile(UUID.fromString("5045bb4a-184e-4bf4-a8fa-7f7c05dd3c77"), "x3th0_"));
+        fake_player.copyLocationAndAnglesFrom(mc.player);
+        fake_player.rotationYawHead = mc.player.rotationYawHead;
+        mc.world.addEntityToWorld(-100, fake_player);
+    }
+
+    @Override
+    protected void disable() {
+        try {
+            mc.world.removeEntity(fake_player);
+        } catch (Exception ignored) {}
+    }
+
+    @Override
+    public void log_out() {
+        this.set_disable();
+    }
+
+    @EventHandler
+    private final Listener<EntityJoinWorldEvent> on_world_event = new Listener<>(p_Event ->
+    {
+        if (p_Event.getEntity() == mc.player)
+        {
+            this.set_disable();
+        }
+    });
+
+}
