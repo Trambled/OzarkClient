6a1108bbee141eac422b9e98ea402b3e282ee091
x3th0 <71413668+x3th0@users.noreply.github.com>
2021-06-21 09:55:08 -0400

    Revert "Delete AutoKitCommand.java"
    This reverts commit 93b8585db8ce816e46b932904234d1280371b9d9.

---
+++ src/main/java/me/trambled/ozark/ozarkclient/command/commands/AutoKitCommand.java
@@ -0,0 +1,42 @@
+package me.trambled.ozark.ozarkclient.command.commands;
+
+import com.mojang.realmsclient.gui.ChatFormatting;
+import me.trambled.ozark.Ozark;
+import me.trambled.ozark.ozarkclient.command.Command;
+import me.trambled.ozark.ozarkclient.util.AutoKitUtil;
+import me.trambled.ozark.ozarkclient.util.MessageUtil;
+
+public class AutoKitCommand extends Command {
+
+    public AutoKitCommand() {
+        super("autokit", "chooses a custom kit");
+    }
+
+    public boolean get_message(String[] message) {
+
+        if (message.length == 1) {
+            MessageUtil.send_client_error_message("message needed");
+            return true;
+        }
+
+        if (message.length >= 2) {
+            StringBuilder kit = new StringBuilder();
+            boolean flag = true;
+            for (String word : message) {
+                if (flag) {
+                    flag = false;
+                    continue;
+                }
+                kit.append(word).append(" ");
+            }
+            AutoKitUtil.set_message(kit.toString());
+            MessageUtil.send_client_message("kit changed to " + ChatFormatting.BOLD + kit.toString());
+            Ozark.get_config_manager().save_settings();
+            return true;
+        }
+
+        return false;
+
+    }
+
+}
