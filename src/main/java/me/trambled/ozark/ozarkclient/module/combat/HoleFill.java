c22986cbd6ee3c5dc6e9e4845d39dba63b32a9af
ProfKambing <fawwaziqbal2008@gmail.com>
2021-06-29 15:37:03 +0800

    um no

--- src/main/java/me/trambled/ozark/ozarkclient/module/combat/HoleFill.java
+++ src/main/java/me/trambled/ozark/ozarkclient/module/combat/HoleFill.java
@@ -16,17 +16,17 @@ import net.minecraft.util.math.BlockPos;
 import java.util.ArrayList;
 
 public class HoleFill extends Module {
-    
-    public HoleFill() {
-		super(Category.COMBAT);
 
-		this.name        = "HoleFill"; 
-		this.tag         = "HoleFill";
-		this.description = "Turn holes into floors.";
+    public HoleFill() {
+        super(Category.COMBAT);
+
+        this.name = "HoleFill";
+        this.tag = "HoleFill";
+        this.description = "Turn holes into floors.";
     }
 
     Setting smart_mode = create("Smart Mode", "HoleFillSmart", false);
-    Setting smart_range = create("Smart Range", "HoleFillSmartRange", 2.11, 0,6);
+    Setting smart_range = create("Smart Range", "HoleFillSmartRange", 2.11, 0, 6);
     Setting hole_toggle = create("Toggle", "HoleFillToggle", true);
     Setting hole_rotate = create("Rotate", "HoleFillRotate", true);
     Setting button = create("Button", "HoleFillButton", false);
@@ -37,21 +37,21 @@ public class HoleFill extends Module {
     private final ArrayList<BlockPos> holes = new ArrayList<>();
 
     @Override
-	public void enable() {
-		if (find_in_hotbar() == -1) {
-		    MessageUtil.send_client_error_message("No obby!");
-		    this.set_disable();
+    public void enable() {
+        if (find_in_hotbar() == -1) {
+            MessageUtil.send_client_error_message("No obby!");
+            this.set_disable();
         }
         find_new_holes();
-	}
+    }
 
-	@Override
-	public void disable() {
+    @Override
+    public void disable() {
         holes.clear();
     }
-    
+
     @Override
-	public void update() {
+    public void update() {
 
         if (find_in_hotbar() == -1) {
             MessageUtil.send_client_error_message("No obby!");
@@ -66,6 +66,7 @@ public class HoleFill extends Module {
                 return;
 
             } else {
+                MessageUtil.send_client_message("Filling" + holes.size() + "holes!");
                 find_new_holes();
             }
         }
@@ -91,7 +92,7 @@ public class HoleFill extends Module {
 
                 if (target.isDead || target.getHealth() <= 0) continue;
 
-                if (target.getDistanceSqToCenter(pos) < smart_range.get_value(1)*smart_range.get_value(1)) {
+                if (target.getDistanceSqToCenter(pos) < smart_range.get_value(1) * smart_range.get_value(1)) {
                     do_smart = true;
                 }
             }
@@ -113,7 +114,7 @@ public class HoleFill extends Module {
 
         if (do_smart || !smart_mode.get_value(true)) {
             if (pos_to_fill != null) {
-                if (BlockUtil.placeBlock(pos_to_fill, find_in_hotbar(), hole_rotate.get_value(true), hole_rotate.get_value(true), swing,ghost_mode.get_value(true) )) {
+                if (BlockUtil.placeBlock(pos_to_fill, find_in_hotbar(), hole_rotate.get_value(true), hole_rotate.get_value(true), swing, ghost_mode.get_value(true))) {
                     holes.remove(pos_to_fill);
                 }
             }
@@ -140,12 +141,12 @@ public class HoleFill extends Module {
 
             boolean possible = true;
 
-            for (BlockPos seems_blocks : new BlockPos[] {
-            new BlockPos( 0, -1,  0),
-            new BlockPos( 0,  0, -1),
-            new BlockPos( 1,  0,  0),
-            new BlockPos( 0,  0,  1),
-            new BlockPos(-1,  0,  0)
+            for (BlockPos seems_blocks : new BlockPos[]{
+                    new BlockPos(0, -1, 0),
+                    new BlockPos(0, 0, -1),
+                    new BlockPos(1, 0, 0),
+                    new BlockPos(0, 0, 1),
+                    new BlockPos(-1, 0, 0)
             }) {
                 Block block = mc.world.getBlockState(pos.add(seems_blocks)).getBlock();
 
