10ab2304e5a5bd33d7f55acf3c749263d02ee64e
Trambled <67767001+Trambled@users.noreply.github.com>
2021-07-08 22:36:30 -0400

    removed padding

--- src/main/java/me/trambled/ozark/ozarkclient/guiscreen/hud/pinnables/Info.java
+++ src/main/java/me/trambled/ozark/ozarkclient/guiscreen/hud/pinnables/Info.java
@@ -14,7 +14,6 @@ public class Info extends Pinnable {
 
 	@Override
 	public void render() {
-		int padding = 5;
 		int nl_r = Ozark.get_setting_manager().get_setting_with_tag("HUD", "HUDStringsColorR").get_value(1);
 		int nl_g = Ozark.get_setting_manager().get_setting_with_tag("HUD", "HUDStringsColorG").get_value(1);
 		int nl_b = Ozark.get_setting_manager().get_setting_with_tag("HUD", "HUDStringsColorB").get_value(1);
