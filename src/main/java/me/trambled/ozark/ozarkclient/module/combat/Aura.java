59b15d0a107c61d2d09ac828ba99f70f8d5685a7
ProfKambing <fawwaziqbal2008@gmail.com>
2021-08-20 09:29:01 +0800

    test build again

--- src/main/java/me/trambled/ozark/ozarkclient/module/combat/Aura.java
+++ src/main/java/me/trambled/ozark/ozarkclient/module/combat/Aura.java
@@ -168,7 +168,7 @@ public class Aura extends Module {
 	public boolean is_compatible(Entity entity) {
 		// Instend entity with some type entity to continue or no.
 		if (player.get_value(true) && entity instanceof EntityPlayer) {
-			if (entity != mc.player && !(entity.getName().equals(mc.player.getName())) /* && WurstplusFriendManager.is_friend(entity) == false */) {
+			if (entity != mc.player && !(entity.getName().equals(mc.player.getName())) /* && FriendManager.is_friend(entity) == false */) {
 				return true;
 			}
 		}
